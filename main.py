f76fb885dfbd41bc8e17747e8c6f5ffac5dfee26
imranshabbir-git <imranshabbir.forextrader@gmail.com>
2026-09-19 03:38:15 +0500

    first code

---
+++ main.py
@@ -0,0 +1,61 @@
+with open("students.txt", "a") as file:
+    file.write("Arshad\n")
+
+
+# with open("students.txt", "w") as file:
+#     file.write("Imran")
+
+
+
+# with open("data.txt", "r") as file:
+#     for line in file: 
+#         print(line)
+
+
+# with open("data.txt", "r") as file:
+#     content = file.readlines()    
+#     print(content)
+    
+
+
+# with open("data.txt", "r") as file:
+#     content1 = file.readline()
+#     content2 = file.readline()
+#     content3 = file.readline()
+#     print(content1)
+#     print(content2)
+#     print(content3)
+
+
+# with open("data.txt", "r") as file:
+#     content1 = file.readline()    
+#     print(content1)
+    
+          
+
+# with open("data.txt", "r") as file:
+#     content = file.read()
+#     print(content)
+
+
+
+
+# file = open("data.txt", "r")
+# content = file.read()
+# print(content)
+# file.close()
+
+
+
+
+# open("filename", "mode")
+
+# open("data.txt", "r") # read
+# open("data.txt", "w") # write
+# open("data.txt", "a") # append
+# open("data.txt", "x") # new file create
+
+
+
+
+
